706fe770ff4d145a89a6cbc847d642c4d44d3495
Hazzim-Escarcega <said-hazzim.escarcega@atos.net>
2022-05-11 09:39:22 -0600

    Video_8

--- src/main/java/atos/upgrade/colmenaapi/entities/Autor.java
+++ src/main/java/atos/upgrade/colmenaapi/entities/Autor.java
@@ -1,34 +1,30 @@
 package atos.upgrade.colmenaapi.entities;
 
-import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.envers.Audited;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.envers.Audited;
 
 @Entity
-@Table(name = "persona")
-@NoArgsConstructor
-@AllArgsConstructor
+@Table(name = "autor")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Audited
-
-public class Persona extends Base {
+public class Autor extends Base {
     @Column(name = "nombre")
     private String nombre;
     @Column(name = "apellido")
     private String apellido;
-    @Column(name = "dni")
-    private int dni;
-
+    @Column( name = "biografia", length = 1500)
+    private String biografia;
 }
+    
+
